931d106d56d9db46f1980181bfa6624c88f9aaef
raghavr93 <raghav.rastogi.utd@gmail.com>
2020-04-13 04:19:54 +0530

    Cities API Project Created

---
+++ CityInfo/Controllers/CitiesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CityInfo.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(
+                new List<object>
+                {
+                    new {id = 1, Name = "Pune"},
+                    new {id = 2, Name = "Mumbai"}
+                }); 
+        }
+    }
+}
